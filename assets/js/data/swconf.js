ea3469278ce4be079bbd42019e942ad58c5629be
Luis <laleza@axuquer.es>
2024-12-08 22:45:23 +0000

    Hola Mundo

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733179664',resources: [
+    cacheName: 'chirpy-1733697922',resources: [
       '/rx/assets/css/jekyll-theme-chirpy.css',
       '/rx/',
       
